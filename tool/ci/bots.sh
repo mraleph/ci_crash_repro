80a84c49208dafaf5d9cd1f2decd760363a10538
Vyacheslav Egorov <vegorov@google.com>
2025-08-13 14:48:17 +0200

    Try crash dump

--- tool/ci/bots.sh
+++ tool/ci/bots.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
-set -ex
+set -x
+
+ulimit -c unlimited
+sudo rm -rf /cores/*
+sudo chmod -R +rwx /cores
 
 echo Running
 
 which dart
 
+dartPath=$(dart tool/ci/print_dart_path.dart)
+sdkBinPath=$(dirname $dartPath)
+
+/usr/libexec/PlistBuddy -c "Add :com.apple.security.get-task-allow bool true" segv.entitlements
+
+codesign -vvv -s - -f --entitlements segv.entitlements --xml $sdkBinPath/dart
+codesign -vvv -s - -f --entitlements segv.entitlements --xml $sdkBinPath/dartvm
+
 dart --version
 
+dart bin/main.dart
 
-for i in {1..200}
-do
-  rm -rf .dart_tool
-  dart pub get
-  dart run build_runner build --delete-conflicting-outputs
-done
+ls -al /cores/*
+
+lldb -s tool/ci/dump_threads.lldb --batch $sdkBinPath/dartvm -c /cores/core.*
+
