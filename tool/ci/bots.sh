253aaf74db6fd98f2765ed2cb7f6fc388ebc4938
Vyacheslav Egorov <vegorov@google.com>
2025-08-13 15:04:44 +0200

    Add comments

--- tool/ci/bots.sh
+++ tool/ci/bots.sh
@@ -2,27 +2,33 @@
 
 set -x
 
+# Enable core dumps
 ulimit -c unlimited
+
+# Remove all existing core dumps
 sudo rm -rf /cores/*
+
+# Make directory which contains core dumps writable.
 sudo chmod -R +rwx /cores
 
-echo Running
+# Identify where dart SDK resides
+DART_BINARY_PATH=$(dart tool/ci/print_dart_path.dart)
+DART_SDK_PATH=$(dirname $DART_BINARY_PATH)
 
-which dart
+# Resign dart and dartvm binaries to give them get-task-allow entitlement.
+# Needed for coredump creation to happen successfully.
+codesign -vvv -s - -f --entitlements tool/ci/dump.entitlements --xml $DART_SDK_PATH/dart
+codesign -vvv -s - -f --entitlements tool/ci/dump.entitlements --xml $DART_SDK_PATH/dartvm
 
-dartPath=$(dart tool/ci/print_dart_path.dart)
-sdkBinPath=$(dirname $dartPath)
-
-/usr/libexec/PlistBuddy -c "Add :com.apple.security.get-task-allow bool true" segv.entitlements
-
-codesign -vvv -s - -f --entitlements segv.entitlements --xml $sdkBinPath/dart
-codesign -vvv -s - -f --entitlements segv.entitlements --xml $sdkBinPath/dartvm
-
+# Check that dart binary still runs.
 dart --version
 
+# Run a script that crashes.
 dart bin/main.dart
 
+# Check if new core files appeared
 ls -al /cores/*
 
+# Dump information from the core (we assume there is just one).
 lldb -s tool/ci/dump_threads.lldb --batch $sdkBinPath/dartvm -c /cores/core.*
 
